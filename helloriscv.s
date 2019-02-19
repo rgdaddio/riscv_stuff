254a06923739d4186c08b0fb7222187f74c7f0ec
rgdaddio <rgdaddio@gmail.com>
2019-02-18 22:16:32 -0500

    Add files via upload

---
+++ helloriscv.s
@@ -0,0 +1,35 @@
+########;as --gdwarf-2 hello_world_arm.s -o hello_world_arm.o
+########;ld -s -o hello_world hello_world.o
+########;Using GNU LD and GNU AS
+########;Port from old Openrisc code the old hello world standby 
+
+
+
+	.data
+
+myStr:
+	.string "hello riscv new world!!\n" #;basic string data
+
+	len = . - myStr                 #;get the length via intrinsic
+
+	.text
+
+	.global _start                  #;set up a start routine
+	.type _start, @function
+
+_start:	
+	
+	li a0, 0 			#stdout
+	lui a1, %hi(myStr)		#high part of message
+	addi a1, a1, %lo(myStr)		#low part of messate
+	li a2, len			#len
+	li a3, 0			#0-out a3
+	li a7, 64			#load syscall number
+	ecall
+
+	li a0, 0
+	li a1, 0
+	li a2, 0
+	li a3, 0
+	li a7, 93			#exit system call
+	ecall
