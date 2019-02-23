561d895fbd8be5e84aca4b143275844d3895bf16
rgdaddio <rgdaddio@gmail.com>
2019-02-22 23:17:55 -0500

    Add files via upload

---
+++ helloriscv_t.s
@@ -0,0 +1,46 @@
+########;riscv64-unknown-elf-as helloriscv_t.s -o helloriscv_t.o
+########;riscv64-unknown-elf-ld -o ttest helloriscv_t.o
+#or
+########;riscv64-unknown-linux-gnu-as helloriscv_t.s -o helloriscv_t.riscv64
+########;risc64-unknown-linux-gnu-ld -o ttest helloriscv_t.o
+########;Run using Spike Sim: spike pk ttest 
+########;Port from old Openrisc code the old hello world standby 
+
+
+
+	.data
+
+myStr:
+	.string "hello riscv new world!!\n" 	#;basic string data
+	len = . - myStr                 	#;get the length via intrinsic
+
+
+	.text
+
+
+
+	.global _start				#;linker start point
+	.type _start, @function
+	
+
+_start:	
+
+main:	
+	
+	li a0, 2 			#stdout
+	lui a1, %hi(myStr)		#high part of message
+	addi a1, a1, %lo(myStr)		#low part of message
+	li a2, len			#len
+	li a3, 0			#0-out a3
+	li a7, 64			#load syscall number
+	ecall
+
+	li a0, 0
+	li a1, 0
+	li a2, 0
+	li a3, 0
+	li a7, 93			#exit system call
+	ecall
+
+	.global main                  #;main routine for Spike PK
+	.type main, @function
