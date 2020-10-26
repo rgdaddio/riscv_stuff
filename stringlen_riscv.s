4698ade0bfafc230ae7a439b6fd8f9376820caeb
rgdaddio@gmail.com <rgdaddio@gmail.com>
2020-10-26 10:47:35 -0400

    Update & clean

--- stringlen_riscv.s
+++ stringlen_riscv.s
@@ -1,15 +1,36 @@
-###########################################################################
+################################################################################
+#Small string length test code. Some extra function call for the hell of it
+#For use in spike the risc5 simulator
+
+#test run 	
+#riscv64-unknown-linux-gnu-as stringlen_riscv.s -o stringlen_riscv.o
+#riscv64-unknown-linux-gnu-ld -o strlen stringlen_riscv.o
+#spike pk strlen
+#
+#The norelax option can probably be removed on real linux	
+################################################################################
 			.data
 testgreeting:
-	.string "strlntst\n"
+	.string "Simple string length test\n"
 	glen = . - testgreeting
-#Note Risc5/binutils ld seems to have problems with multiple symbols and gp
-############################################################################	
+
+			.option norelax #kill the evil gp
+
+testit:
+	.string "\nlength\n"
+	glen = . - testit
+			.option norelax
+
+resultstr:
+	.string " is the length of test string\n\n"
+	rlen = . - resultstr
+		
+################################################################################	
 			.text
-	.global _start                  #;set up a start routine
+	.global _start                  	#;set up a start routine
 	.type _start, @function
 
-##############################################################################	
+################################################################################	
 	_start:
 	li a0, 1 				#stdout
 	lui a1, %hi(testgreeting)		#high part of message
@@ -19,23 +40,28 @@ testgreeting:
 	li a7, 64				#load syscall number
 	ecall
 
-	jal get_test_string			#simple function
-	jal string_len				#get the len of testgreeting string
+	lui a1, %hi(testit)			#high part of message
+	addi a1, a1, %lo(testit)		#low part of message
+	
+	jal string_len				#get the len of testit string
 
 	li a0, 1
-	addi sp, sp, 8
-	mv a1, sp
 	li a2, 1
 	li a3, 0
 	li a7, 64
 	ecall					#print the length up to 9 chars
+
+	jal results
+
 	jal riscv_exit
+
 #################################################################################
 .global string_len
 	.type string_len,%function
 string_len:
-	mv t3, a0
-	li t6, 0  #counter                     #we only count to 9 for test
+	mv t3, a1
+	li t6, 0  #counter                     	#we only count to 9 for test
+						#itoa TBD... :)
 not_done:
 	lb t4, 0(t3)
 	beq t4, x0, done			#find \0 termination
@@ -44,14 +70,22 @@ not_done:
 	j not_done				#loop till done
 done:
 	addi t6, t6, 0x30
-	sb t6, 8(sp)				#save the count on the stack
+	sb t6, 0(a1)
 	ret
+
 ################################################################################
-.global get_test_string
-	.type get_test_string,%function
-get_test_string:	
-	mv a0, a1				#move the string ptr to a0
+.global results
+	.type results,%function
+results:	
+	li a0, 1 				#stdout
+	lui a1, %hi(resultstr)			#high part of message
+	addi a1, a1, %lo(resultstr)		#low part of messate
+	li a2, rlen				#len
+	li a3, 0				#0-out a3
+	li a7, 64
+	ecall
 	ret
+
 ################################################################################	
 .global riscv_exit
 	.type riscv_exit,%function
