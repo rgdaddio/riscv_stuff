b90ed2a3b8610015fbd484e5d076872662190470
rgdaddio@gmail.com <rgdaddio@gmail.com>
2020-11-13 00:57:07 -0500

    Update

--- simple_riscv_setup_hifive.s
+++ simple_riscv_setup_hifive.s
@@ -36,19 +36,19 @@ _start:
 	#whining linker
 
 	# setup stacks per hart
-   csrr t0, mhartid                # read current hart id
-   slli t0, t0, 10                 # shift left the hart id by 1024
-   la   sp, stacks + STACK_SIZE    # set the initial stack pointer 
-                                   # to the end of the stack space
-   add  sp, sp, t0                 # move the current hart stack pointer
-                                   # to its place in the stack space
+   csrr t0, mhartid                	# read current hart id
+   slli t0, t0, 10                 	# shift left the hart id by 1024
+   la   sp, stacks + STACK_SIZE    	# set the initial stack pointer 
+        				# to the end of the stack space
+   add  sp, sp, t0                 	# move the current hart stack pointer
+        				# to its place in the stack space
 
     # park harts with id != 0
-    csrr a0, mhartid                # read current hart id
-    bnez a0, park                   # if we're not on the hart 0
-                                    # we park the hart
+    csrr a0, mhartid			# read current hart id
+    bnez a0, park                   	# if we're not on the hart 0
+        				# we park the hart
 
-    j    _enter                      # jump to c
+    j    _enter                      	# jump to assembly _enter
 
 park:
     wfi
