7cf079a7bef0adcb34cfdca00be8af7822027812
rgdaddio@gmail.com <rgdaddio@gmail.com>
2020-11-26 21:35:34 -0500

    Update

--- hifive/simple_riscv_test_driver_hifive.s
+++ hifive/simple_riscv_test_driver_hifive.s
@@ -1,4 +1,3 @@
-.equ UARTBASE, 0x10013000
 .equ STACK_SIZE, 1024
 .equ MEM_BASE, 0x80000000
 .equ MEM_SIZE, 0x4000	
@@ -6,8 +5,13 @@
 		.section 	.rodata
 
 testgreeting:
-		.string "\nhellostring\n"
+		.string "\narray_test\n"
 		glen = . - testgreeting
+
+.align 2 #2**2	
+array_initialized:
+.word 0x35,0x33,0x36,0x37,0x32,0x38,0x37,0x31,0x39,0x35	
+	
 
 # Using hardware detection and stack sizing per Hart. Trick code into thinking
 # it is going to 'c' when it is really going to Assembly _enter...
@@ -43,4 +47,24 @@ stacks:
 	.skip STACK_SIZE * 4            # allocate
 
 
-	
+.globl _enter
+.type _enter,@function
+		
+_enter:							
+	jal		inituart			#; initialize the uart
+	lui		a1, %hi(testgreeting)		#; move upper 16 in
+        addi 		a1, a1, %lo(testgreeting)	#; add in the lower
+	li 		a2, glen			#; use the const string len
+	li		a3, 1				#; set a counter to 1
+
+spin:	
+	lb		a0, 0(a1)			#; get a byte from greetinn
+	jal 		txuart				#; print the byte to uart
+	jal             delayit				#; let uart catch up
+	beq		a3, a2, done			#; get out if cntr reach len
+	addi 		a3, a3, 1			#; incr counter
+	addi 		a1, a1, 1			#; incr string pointer
+
+	j		spin
+done:
+	ret
